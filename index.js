342dd8c9dd987adac24258066e2ace96cf69ca3a
befierce <enterbhatt123@gmail.com>
2023-07-09 16:32:43 +0530

    breakdown of code to understand better

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
 var form = document.getElementById('details');
-//add event listener
 form.addEventListener('submit', addUser);
-//giving functionality
-function addUser(e) {
+
+function addUser(e){
     e.preventDefault();
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
-    var number = document.getElementById('Number').value;
+    var number = document.getElementById('number').value;
 
-    var userDetails = {
-        email: email,
-        name: name,
-        number: number
-    };
-    var userDetailsString = JSON.stringify(userDetails);
-    var userId = generateUserId();
-    localStorage.setItem(userId, userDetailsString);
+    //<!--SAVING DATA TO LOCAL STORATGE-->//
+    var userData = {name: name,
+                    email: email,
+                    number: number};
+    var userDataString = JSON.stringify(userData);
+    localStorage.setItem("user",userDataString);
+    //--------------------------------------//
 
+    //<!--breakdown of ADDING USER DATA TO LIST ITEMS-->//
+    //li elemtnt create kia document mai
     var li = document.createElement('li');
+    //usko classname diya
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(name));
-    li.appendChild(document.createElement('br'));
+    //variable liya usme text node create kiya
+    var liname = document.createTextNode(name);
+    //us variable ko li ka child bna diya means text node ko
+    //li mai as a child create kr diai
+    li.appendChild(liname);
+    li.appendChild(document.createElement('br'));//is code ka
+    //breakdown same hai
     li.appendChild(document.createTextNode(email));
     li.appendChild(document.createElement('br'));
     li.appendChild(document.createTextNode(number));
     li.appendChild(document.createElement('br'));
 
-
-
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function () {
-        // Remove user data from local storage
-        localStorage.removeItem(userId);
-        // Remove user item from the displayed list
-        li.remove();
-    });
-    li.appendChild(deleteButton);
-    var userList = document.getElementById('users');
-    userList.appendChild(li);
-}
-function generateUserId() {
-    // Implement your logic to generate a unique identifier
-    // This can be a random string, a timestamp, or any other unique value
-    // Return the generated identifier
-    return "user_" + Math.random().toString(36).slice(2, 11);
 }
