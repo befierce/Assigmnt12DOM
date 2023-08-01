16ad5dc938df22d29ebc853576ec079a30af1884
befierce <enterbhatt123@gmail.com>
2023-08-01 10:47:15 +0530

    editbutton is functional updated data on crud crud

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
 // JavaScript code
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/ed2cdcf07b7a489bbf767198d25a7f7c/AppointmentData")
-        .then((response) => {
-            for (var i = 0; i < response.data.length; i++) {
-                displayDataOnScreen(response.data[i]);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    fetchAppointmentData();
 });
 
 var form = document.getElementById('details');
-form.addEventListener('submit', addUser);
+form.addEventListener('submit', submitUserData);
 
-function addUser(e) {
-    e.preventDefault();
+function submitUserData(e) {
+    e.preventDefault();//fetiching data  from input fields and giving it to the variables
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var number = document.getElementById('number').value;
 
-    var userData = {
+    var userData = {//making object of the data
         name: name,
         email: email,
         number: number
     };
 
     // Making network call to add a new user to server database
-    axios.post("https://crudcrud.com/api/ed2cdcf07b7a489bbf767198d25a7f7c/AppointmentData", userData)
+    axios.post("https://crudcrud.com/api/bb37140fe0464c36bb065efe47ca864c/AppointmentData", userData)
         .then((response) => {
-            displayDataOnScreen(response.data);//here we are giving data to the function and it is 
-            //extracting the user id to make list element of pirticular id
+            displayDataOnScreen(response.data);//displaying sbmitted data on the screen
         })
         .catch((err) => {
             console.log(err);
@@ -41,12 +32,37 @@ function addUser(e) {
 }
 
 function deleteUser(event) {
-    var li = event.target.parentElement; // Get the parent list item (li)
-    var userID = li.getAttribute('data-user-id'); // Get the user's ID from the data attribute
+    var li = event.target.parentElement;
+    var userID = li.getAttribute('data-user-id');
 
-    axios.delete(`https://crudcrud.com/api/ed2cdcf07b7a489bbf767198d25a7f7c/AppointmentData/${userID}`)
+    // Making axios request to delete details from server
+    axios.delete(`https://crudcrud.com/api/bb37140fe0464c36bb065efe47ca864c/AppointmentData/${userID}`)
         .then(() => {
             li.remove();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function editdetails(event) {
+    var li = event.target.parentElement;
+    var userID = li.getAttribute('data-user-id');//taking user id form parent 
+    //parent here is <li class="list-group-item" data-user-id = _userID >Dummy USER</li>
+
+    // Fetch the user data from server based on userID
+    axios.get(`https://crudcrud.com/api/bb37140fe0464c36bb065efe47ca864c/AppointmentData/${userID}`)
+        .then((response) => {
+            var userData = response.data;
+            document.getElementById('name').value = userData.name;
+            document.getElementById('email').value = userData.email;
+            document.getElementById('number').value = userData.number;
+
+            // Delete the existing user data from the browser and server
+            axios.delete(`https://crudcrud.com/api/bb37140fe0464c36bb065efe47ca864c/AppointmentData/${userID}`)
+                .catch((error) => {
+                    console.log(error);
+                });
         })
         .catch((error) => {
             console.log(error);
@@ -61,13 +77,14 @@ function displayDataOnScreen(userData) {
     var email = userData.email;
     var number = userData.number;
     var userID = userData._id; // Assuming the server returns the ID as "_id". Change this according to your API.
-/** <li class="list-group-item" data-user-id="user-12345">
-    John Doe<br>
-    john.doe@example.com<br>
-    123-456-7890<br>
-    <button>remove</button>
-    </li> */
+
     li.setAttribute('data-user-id', userID); // Add the user's ID as a data attribute to the list item (li)
+    /*demonstration
+    
+    <ul id="users" class="list-group">
+        <li class="list-group-item" data-user-id = _userID >Dummy USER</li>
+    
+    */
 
     var liname = document.createTextNode(name);
     li.appendChild(liname);
@@ -84,6 +101,23 @@ function displayDataOnScreen(userData) {
     deleteButton.addEventListener('click', deleteUser); // Attach the deleteUser function here
     li.appendChild(deleteButton);
 
+    var editButton = document.createElement('button');
+    editButton.textContent = 'edit'; // Corrected the textContent property for editButton
+    editButton.addEventListener('click', editdetails);
+    li.appendChild(editButton);
+
     var userList = document.getElementById('users');
     userList.appendChild(li);
+}
+
+function fetchAppointmentData() {
+    axios.get("https://crudcrud.com/api/bb37140fe0464c36bb065efe47ca864c/AppointmentData")
+        .then((response) => {
+            for (var i = 0; i < response.data.length; i++) {
+                displayDataOnScreen(response.data[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
